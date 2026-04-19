67cfa15528aec734a24dda0e1ec440b79f640868
MartinKalema <kalemaaaaaaaa@gmail.com>
2026-04-19 23:21:28 +0900

    refactor(persistence): drop replication frontier key version suffix

--- crates/common/src/persistence.rs
+++ crates/common/src/persistence.rs
@@ -191,7 +191,7 @@ impl From<PersistenceGlobalKey> for String {
                 "document_confirmed_deleted_ts".to_string()
             },
             PersistenceGlobalKey::MaxRepeatableTimestamp => "max_repeatable_ts".to_string(),
-            PersistenceGlobalKey::ReplicationFrontiers => "replication_frontiers_v1".to_string(),
+            PersistenceGlobalKey::ReplicationFrontiers => "replication_frontiers".to_string(),
             PersistenceGlobalKey::TableSummary => "table_summary_v2".to_string(),
             PersistenceGlobalKey::TablesByIdIndex => "tables_by_id".to_string(),
             PersistenceGlobalKey::IndexByIdIndex => "index_by_id".to_string(),
@@ -211,7 +211,7 @@ impl FromStr for PersistenceGlobalKey {
             "document_min_snapshot_ts" => Ok(Self::DocumentRetentionMinSnapshotTimestamp),
             "document_confirmed_deleted_ts" => Ok(Self::DocumentRetentionConfirmedDeletedTimestamp),
             "max_repeatable_ts" => Ok(Self::MaxRepeatableTimestamp),
-            "replication_frontiers_v1" => Ok(Self::ReplicationFrontiers),
+            "replication_frontiers" => Ok(Self::ReplicationFrontiers),
             "table_summary_v2" => Ok(Self::TableSummary),
             "tables_by_id" => Ok(Self::TablesByIdIndex),
             "tables_table_id" => Ok(Self::TablesTabletId),
